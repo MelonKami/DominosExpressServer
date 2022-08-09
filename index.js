19637a66a5e18c84e269a71ec6a320b3791f8102
MelonKami <marius.berg.lindegaard@gmail.com>
2022-08-09 23:17:12 +0200

    test

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ app.post('/postAnnouncement', function (req, res) {
     announcements.announcements.push({ date: Date(), text: req.body })
 
     console.log('saving announcement')
-    fs.writeFileSync('./announcements.json', announcements["announcements"] = { date: Date(), text: req.body })
+    fs.writeFileSync('./announcements.json', announcements)
 
     // add announcement to announcement json file with correct format
     // eg. date, time, message ect.
