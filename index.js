be54b8f8beb36597c238e73e096049b8b4c830bf
Marius Berg Lindegaard <marius.berg.lindegaard@gmail.com>
2024-06-29 21:26:48 +0200

    test

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.get('/', (req, res) => {
 app.get('/getResources', (req, res) => {
     console.log('getResources');
     // return resources
-    console.log('Sending resources: ' + resources)
+    console.log('Sending resources: ' + resources.json())
     res.send(resources);
 });
 
